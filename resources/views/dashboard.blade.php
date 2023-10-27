dbe399022b97d55720723bead727affb7e186d39
monikabothra <04monikabothra@gmail.com>
2023-10-27 17:06:07 +0530

    dashboaed view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,92 +1,31 @@
 @extends('layouts.app')
 @section('content')
-    <div class="row">
+    <div class="container mt-5">
+        <div class="page-header text-center">
+            <h1>Available Cars!</h1>
+        </div>
+        {{-- @dd($car) --}}
 
-        @foreach ($car as $c)
-            {{-- @dd($c) --}}
-            <div class="col-md-2 card " style="width: 18rem;">
-                {{-- <img src="..." class="card-img-top" alt="..."> --}}
-                <div class="card-body">
-                    <h5 class="card-title">Name:{{ $c->name }}</h5>
-                    <p class="card-text">Type:{{ $c->car_type->type }}</p>
-                    <p class="card-text">Speed:{{ $c->speed }}</p>
-                    <p class="card-text">price:{{ $c->price }}</p>
+        <div class="row g-4">
 
-                    <p class="card-text">Owner:{{ $c->owners->name }}</p>
-                    {{-- <p class="card-text">{{ $c->name }}</p> --}}
-                    <a href="car/{{ $c->id }}" class="btn btn-primary car">View Details</a>
+            @forelse ($car as $c)
+                <div class="col-lg-3 col-md-4">
+                    <div class="card">
+                        <div class="card-header text-capitalize">{{ $c->car_type->type }}</div>
+                        <div class="card-body">
+                            <h5 class="card-title text-capitalize">{{ $c->name }}</h5>
+                            <p class="card-text text-capitalize">Speed:{{ $c->speed }}</p>
+                            <p class="card-text text-capitalize">Price:{{ $c->price }}</p>
+                            <p class="card-text text-capitalize">Owner:{{ $c->owners->name }}</p>
+                            <a href="car/{{ $c->id }}" class="btn btn-primary">View Details</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        @endforeach
-    </div>
-    <div class="row">
+            @empty
+                <h5 class="card-title text-capitalize text-center">No Data Available!</h5>
+            @endforelse
 
-        <div class="card-group">
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                        content. This content is a little bit longer.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                        content. This content is a little bit longer.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                        content. This content is a little bit longer.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                        content. This content is a little bit longer.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                        content. This card has even longer content than the first to show that equal height action.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
+
         </div>
     </div>
 @endsection
